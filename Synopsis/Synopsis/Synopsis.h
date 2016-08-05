94027c594d4d82268ef82362d7945649a1417ce2
vade <vade@vade.info>
2016-08-05 18:59:24 -0400

    Initial Commit

---
+++ Synopsis/Synopsis/Synopsis.h
@@ -0,0 +1,19 @@
+//
+//  Synopsis.h
+//  Synopsis
+//
+//  Created by vade on 8/5/16.
+//  Copyright © 2016 v002. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Synopsis.
+FOUNDATION_EXPORT double SynopsisVersionNumber;
+
+//! Project version string for Synopsis.
+FOUNDATION_EXPORT const unsigned char SynopsisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Synopsis/PublicHeader.h>
+
+
