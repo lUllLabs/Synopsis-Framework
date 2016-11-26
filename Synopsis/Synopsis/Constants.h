57c4c67da8544abba1c6b1dadd8a68f5f34826a4
vade <vade@vade.info>
2016-11-26 18:01:19 -0500

    Update constants

--- Synopsis/Synopsis/Constants.h
+++ Synopsis/Synopsis/Constants.h
@@ -13,6 +13,7 @@
 
 // Identifier Synopsis for AVMetadataItems
 extern NSString* const kSynopsislMetadataIdentifier;
+extern NSString* const kSynopsislMetadataIdentifierSortKey;
 
 // Supported Synopsis NSSortDescriptor Keys
 extern NSString* const kSynopsisGlobalMetadataDictKey;
@@ -20,8 +21,11 @@ extern NSString* const kSynopsisFeatureVectorDictKey;
 extern NSString* const kSynopsisPerceptualHashDictKey;
 extern NSString* const kSynopsisDominantColorValuesDictKey;
 extern NSString* const kSynopsisHistogramDictKey;
+extern NSString* const kSynopsisDescriptionDictKey;
 
-// Supported Synopsis Keys
+// Supported Synopsis Sort Keys (these keys cant use the reverse dns notation due to conflicts with key value bs)
+// ie: tld.domain.value is treated as a path, not as a raw string
+// These replace dots with _ so we avoid the issue
 extern NSString* const kSynopsisGlobalMetadataSortKey;
 extern NSString* const kSynopsisFeatureVectorSortKey;
 extern NSString* const kSynopsisPerceptualHashSortKey;
--- Synopsis/Synopsis/Constants.m
+++ Synopsis/Synopsis/Constants.m
@@ -9,16 +9,18 @@
 #import <Foundation/Foundation.h>
 
 NSString* const kSynopsislMetadataIdentifier = @"mdta/info.synopsis.metadata";
+NSString* const kSynopsislMetadataIdentifierSortKey = @"mdta_info_synopsis_metadata";
 
 // TODO: Should be Standard Analyzer no?
 NSString* const kSynopsisGlobalMetadataDictKey = @"info.Synopsis.OpenCVAnalyzer";
-NSString* const kSynopsisPerceptualHashDictKey = @"Hash";
+NSString* const kSynopsisPerceptualHashDictKey = @"PerceptualHash";
 NSString* const kSynopsisFeatureVectorDictKey = @"Features";
 NSString* const kSynopsisDominantColorValuesDictKey = @"DominantColors";
 NSString* const kSynopsisHistogramDictKey = @"Histogram";
+NSString* const kSynopsisDescriptionDictKey = @"Description";
 
 
-NSString* const kSynopsisGlobalMetadataSortKey = @"info_Synopsis_OpenCVAnalyzer";
+NSString* const kSynopsisGlobalMetadataSortKey = @"info_synopsis_opencvanalyzer";
 NSString* const kSynopsisFeatureVectorSortKey = @"info_synopsis_feature_vector";
 NSString* const kSynopsisPerceptualHashSortKey = @"info_synopsis_perceptual_hash";
 NSString* const kSynopsisDominantColorValuesSortKey = @"info_synopsis_dominant_colors";
