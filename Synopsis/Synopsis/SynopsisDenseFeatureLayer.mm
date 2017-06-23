a19bc92a3c01d27d8e3be87ebc141958118aa78e
vade <vade@vade.info>
2017-06-22 21:49:59 -0400

    Kind of working feature vector visualizer

--- Synopsis/Synopsis/SynopsisDenseFeatureLayer.mm
+++ Synopsis/Synopsis/SynopsisDenseFeatureLayer.mm
@@ -17,53 +17,73 @@
     self = [super init];
     if(self)
     {
+        NSDictionary* actions = @{@"contents" : [NSNull null] , @"frame" : [NSNull null], @"position" : [NSNull null], @"frameSize" : [NSNull null], @"frameOrigin" : [NSNull null], @"bounds" : [NSNull null]};
+        self.actions = actions;
     }
     return self;
 }
 
 - (void) drawInContext:(CGContextRef)ctx
 {
-    
-    cv::Mat mat = self.feature.cvMatValue;
-    cv::Mat normalized;
-    cv::normalize(mat, normalized);
-    
-    normalized.convertTo(normalized, CV_8UC4);
-    
-//    int depth = normalized.depth();
-//    int channels = normalized.channels();
-    
-    int width = normalized.cols;
-    int height = normalized.rows;
-    
-    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
-                              [NSNumber numberWithBool:YES], kCVPixelBufferCGImageCompatibilityKey,
-                              [NSNumber numberWithBool:YES], kCVPixelBufferCGBitmapContextCompatibilityKey,
+    if(self.feature)
+    {
+        cv::Mat mat = self.feature.cvMatValue;
+        if(!mat.empty())
+        {
+            cv::Mat normalized;
+            //    cv::normalize(mat, normalized);
+            
+            mat.convertTo(normalized, CV_8UC1);
+            
+            normalized *= 255.0;
+            
+            //    int depth = normalized.depth();
+            //    int channels = normalized.channels();
+            
+            normalized = normalized.t();
+            
+            int width = normalized.cols;
+            int height = normalized.rows;
+            
+            NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+                                     [NSNumber numberWithBool:YES], kCVPixelBufferCGImageCompatibilityKey,
+                                     [NSNumber numberWithBool:YES], kCVPixelBufferCGBitmapContextCompatibilityKey,
 #if TARGET_OS_OSX
-                             [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLCompatibilityKey,
-                             [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLTextureCacheCompatibilityKey,   
-                             [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLCompatibilityKey,
+                                     [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLCompatibilityKey,
+                                     [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLTextureCacheCompatibilityKey,
+                                     [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLCompatibilityKey,
 #else
-                             [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLESCompatibilityKey,
-                             [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLESTextureCacheCompatibilityKey,
-                             [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLESCompatibilityKey,
+                                     [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLESCompatibilityKey,
+                                     [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLESTextureCacheCompatibilityKey,
+                                     [NSNumber numberWithBool:YES], kCVPixelBufferOpenGLESCompatibilityKey,
 #endif
-                             @{}, kCVPixelBufferIOSurfacePropertiesKey,
-                             [NSNumber numberWithInt:width], kCVPixelBufferWidthKey,
-                             [NSNumber numberWithInt:height], kCVPixelBufferHeightKey,
-                             nil];
-    
-    CVPixelBufferRef imageBuffer;
-    CVReturn status = CVPixelBufferCreate(kCFAllocatorMalloc, width, height, kCVPixelFormatType_32BGRA, (__bridge CFDictionaryRef) (options), &imageBuffer) ;
-    
-    NSParameterAssert(status == kCVReturnSuccess && imageBuffer != NULL);
-    
-    CVPixelBufferLockBaseAddress(imageBuffer, 0);
-    void *base = CVPixelBufferGetBaseAddress(imageBuffer) ;
-    memcpy(base, normalized.data, normalized.total());
-    CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
-    
-    self.contents = (id)CFBridgingRelease(imageBuffer);    
+                                     @{}, kCVPixelBufferIOSurfacePropertiesKey,
+                                     [NSNumber numberWithInt:width], kCVPixelBufferWidthKey,
+                                     [NSNumber numberWithInt:height], kCVPixelBufferHeightKey,
+                                     nil];
+            
+            CVPixelBufferRef imageBuffer;
+            CVReturn status = CVPixelBufferCreate(kCFAllocatorMalloc, width, height, kCVPixelFormatType_OneComponent8, (__bridge CFDictionaryRef) (options), &imageBuffer) ;
+            
+            NSParameterAssert(status == kCVReturnSuccess && imageBuffer != NULL);
+            
+            CVPixelBufferLockBaseAddress(imageBuffer, 0);
+            void *base = CVPixelBufferGetBaseAddress(imageBuffer) ;
+            memcpy(base, normalized.data, normalized.total());
+            CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
+            
+            self.contents = (id)CFBridgingRelease(imageBuffer);
+        }
+        else
+        {
+            self.contents = nil;
+        }
+
+    }
+    else
+    {
+        self.contents = nil;
+    }
 }
 
 @end
