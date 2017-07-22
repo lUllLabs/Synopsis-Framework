706f46ba9631663b94a87840a97432ff2df69ea6
vade <vade@vade.info>
2017-07-22 00:01:08 -0400

    Add metadata JSON export options.

--- Synopsis/Synopsis/SynopsisMetadataEncoder.h
+++ Synopsis/Synopsis/SynopsisMetadataEncoder.h
@@ -10,6 +10,21 @@
 #import <AVFoundation/AVFoundation.h>
 #import <CoreMedia/CoreMedia.h>
 
+typedef enum : NSInteger {
+    // Dont write out any JSON
+    SynopsisMetadataEncoderJSONOptionNone = 0,
+    // Single sidecar file
+    SynopsisMetadataEncoderJSONOptionContiguous = 1,
+    // Global metadata only sidecar file (no per frame)
+    SynopsisMetadataEncoderJSONOptionGlobalOnly = 2,
+    // GLobal metadata as well as individial per frame sequences
+    SynopsisMetadataEncoderJSONOptionSequence = 3,
+    // Export training data used to make new dictionary files for ZSTD
+    SynopsisMetadataEncoderJSONOptionZSTDTraining = -1,
+
+} SynopsisMetadataEncoderJSONOption;
+
+
 @protocol SynopsisVersionedMetadataEncoder <NSObject>
 - (AVTimedMetadataGroup*) encodeSynopsisMetadataToTimesMetadataGroup:(NSData*)metadata timeRange:(CMTimeRange)timeRange;
 - (AVMetadataItem*) encodeSynopsisMetadataToMetadataItem:(NSData*)metadata timeRange:(CMTimeRange)timeRange;
@@ -18,9 +33,9 @@
 
 @interface SynopsisMetadataEncoder : NSObject
 @property (readonly) NSUInteger version;
-@property (readonly) BOOL cacheForExport;
+@property (readonly) SynopsisMetadataEncoderJSONOption jsonOption;
 
-- (instancetype) initWithVersion:(NSUInteger)version cacheJSONForExport:(BOOL)cacheJSONForExport;
+- (instancetype) initWithVersion:(NSUInteger)version withJSONOption:(SynopsisMetadataEncoderJSONOption)jsonOption;
 - (AVTimedMetadataGroup*) encodeSynopsisMetadataToTimesMetadataGroup:(NSDictionary*)metadata timeRange:(CMTimeRange)timeRange;
 - (AVMetadataItem*) encodeSynopsisMetadataToMetadataItem:(NSDictionary*)metadata timeRange:(CMTimeRange)timeRange;
 - (NSData*) encodeSynopsisMetadataToData:(NSDictionary*)metadata;
